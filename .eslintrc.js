ce3a95e42af8d0513d4dd47ad8f63f814b2fe685
Jose Quezada <quezada007@yahoo.com>
2019-08-21 21:29:32 -0700

    update eslint and node packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
     "env": {
         "browser": true,
+        "node": true,
         "commonjs": true,
         "es6": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "sourceType": "module"
-    },
+    "extends": "airbnb",
+    "parser": "babel-eslint",
     "rules": {
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": "off"
-    },
-    "globals": {
-        "dataLayer": true
+        "indent": ["warn", 4],
+        "eol-last": "off",
+        "max-len": "off",
+        "comma-dangle": ["error", "never"],
+        "react/jsx-indent": ["warn", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "react/prop-types": "warn",
+        "jsx-a11y/label-has-associated-control": [ 2, {
+            "labelComponents": ["label"],
+            "labelAttributes": ["htmlFor"],
+            "controlComponents": ["input"]
+        }]
     }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+/* eslint-disable */
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/js/main.js',
+    entry: './src/js/App.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].js'
@@ -11,10 +12,13 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 enforce: 'pre',
                 exclude: /node_modules/,
                 loader: 'eslint-loader'
@@ -36,10 +40,33 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
+                    /**
+                     * MiniCssExtractPlugin
+                     * https://webpack.js.org/plugins/mini-css-extract-plugin/
+                     */
                     MiniCssExtractPlugin.loader,
-                    'css-loader?sourceMap',
-                    'postcss-loader?sourceMap',
-                    'sass-loader?sourceMap'
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: [
+                                require('autoprefixer')()
+                            ],
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
                 ]
             },
         ]
